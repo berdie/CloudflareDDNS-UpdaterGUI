87fc30230a16bfc4112377635904d19adeafb6cf
bertuccodiego <44110003+berdie@users.noreply.github.com>
2025-05-12 20:05:21 +0200

    Update Installer

--- obj/Release/net8.0-windows/win-x64/CloudflareDDNS-UpdaterGUI.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/win-x64/CloudflareDDNS-UpdaterGUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CloudflareDDNS-UpdaterGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1859268b76206182ef958c9c9bc6c02a2fa0bc93")]
 [assembly: System.Reflection.AssemblyProductAttribute("CloudflareDDNS-UpdaterGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CloudflareDDNS-UpdaterGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
